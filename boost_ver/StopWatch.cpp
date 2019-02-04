faf18d738a018ad0ab195ead8e613ca777550fc1
qtuancr261 <tuancr261@gmail.com>
2019-02-04 23:54:53 +0700

    StopWatch for boost lib

---
+++ boost_ver/StopWatch.cpp
@@ -0,0 +1,60 @@
+/* 
+ * File:   StopWatch.cpp
+ * Author: KjellKod
+ * From: https://github.com/KjellKod/StopWatch
+ * 
+ * Created on 2014-02-07 
+ */
+
+#include "StopWatch.h"
+StopWatch::StopWatch()
+    : mStart(b_clock::now())
+{
+    static_assert(boost::chrono::steady_clock::is_steady, "Serious OS/C++ library issues. Steady clock is not steady");
+    // FYI:  This would fail  static_assert(std::chrono::high_resolution_clock::is_steady(), "High Resolution Clock is NOT steady on CentOS?!");
+}
+
+StopWatch::StopWatch(const StopWatch& other)
+    : mStart(other.mStart)
+{
+}
+
+/// @return StopWatch::StopWatch&  - assignment operator.
+StopWatch& StopWatch::operator=(const StopWatch& rhs)
+{
+    mStart = rhs.mStart;
+    return *this;
+}
+
+/// @return the elapsed microseconds since start
+uint64_t StopWatch::ElapsedUs() const
+{
+    return boost::chrono::duration_cast<b_microseconds>(b_clock::now() - mStart).count();
+}
+
+/// @return the elapsed milliseconds since start
+uint64_t StopWatch::ElapsedMs() const
+{
+    return boost::chrono::duration_cast<b_milliseconds>(b_clock::now() - mStart).count();
+}
+
+/// @return the elapsed seconds since start
+uint64_t StopWatch::ElapsedSec() const
+{
+    return boost::chrono::duration_cast<b_seconds>(b_clock::now() - mStart).count();
+}
+/**
+ * Resets the start point
+ * @return the updated start point
+ */
+//std::chrono::steady_clock::time_point StopWatch::Restart()
+//{
+//    mStart = b_clock::now();
+//    return mStart;
+//}
+
+boost::chrono::steady_clock::time_point StopWatch::B_Restart()
+{
+    mStart = b_clock::now();
+    return mStart;
+}
---
+++ boost_ver/StopWatch.h
@@ -0,0 +1,39 @@
+/* 
+ * File:   StopWatch.h
+ * Author: KjellKod
+ * From: https://github.com/KjellKod/StopWatch
+ * 
+ * Created on 2014-02-07 
+ */
+
+#ifndef H_STOPWATCH_H
+#define H_STOPWATCH_H
+#include <boost/chrono.hpp>
+#include <chrono>
+class StopWatch {
+public:
+    typedef std::chrono::steady_clock clock;
+    typedef std::chrono::microseconds microseconds;
+    typedef std::chrono::milliseconds milliseconds;
+    typedef std::chrono::seconds seconds;
+    typedef boost::chrono::steady_clock b_clock;
+    typedef boost::chrono::microseconds b_microseconds;
+    typedef boost::chrono::milliseconds b_milliseconds;
+    typedef boost::chrono::seconds b_seconds;
+
+    StopWatch();
+    StopWatch(const StopWatch&);
+    StopWatch& operator=(const StopWatch& rhs);
+
+    uint64_t ElapsedUs() const;
+    uint64_t ElapsedMs() const;
+    uint64_t ElapsedSec() const;
+
+    //std::chrono::steady_clock::time_point Restart();
+    boost::chrono::steady_clock::time_point B_Restart();
+
+private:
+    b_clock::time_point mStart;
+};
+
+#endif // H_STOPWATCH_H
